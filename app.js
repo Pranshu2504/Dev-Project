ca742d732bb17aead3eb44ec2c8da01916030bcb
Pranshu Goel <pranshugoel250405@gmail.com>
2025-07-19 00:22:51 +0530

    Changes

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://dev-project-n2qjkbcvi-pranshu-goels-projects.vercel.app",
+    origin: "https://dev-project-b26zcosxi-pranshu-goels-projects.vercel.app",
     credentials: true,
   })
 );
