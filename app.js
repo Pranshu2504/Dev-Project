6083a5378c0d9bf7b55e079089bd523fc09c04f2
Pranshu Goel <pranshugoel250405@gmail.com>
2025-07-18 14:03:33 +0530

    Updated main server

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://dev-project-hfmnznqja-pranshu-goels-projects.vercel.app",
+    origin: "https://dev-project-7vdy8q7u2-pranshu-goels-projects.vercel.app",
     credentials: true,
   })
 );
